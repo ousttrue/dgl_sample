98e34db530bf7ce1698d77b86e5d38580a2f1832
ousttrue <ousttrue@gmail.com>
2016-09-23 04:14:20 +0900

    refactoring

--- source/app.d
+++ source/app.d
@@ -1,81 +1,8 @@
 module main;
 
-import derelict.opengl3.gl3;
-import derelict.glfw3.glfw3;
-
+import glfw;
 import derelict.imgui.imgui;
 import imgui_glfw: igGlfwGL3;
-
-
-extern(C) nothrow void error_callback(int error, const(char)* description)
-{
-	import std.stdio;
-    import std.conv;
-	try writefln("glfw err: %s ('%s')",error, to!string(description));
-	catch{}
-}
-
-
-class GLFW
-{
-	GLFWwindow *m_window;
-	@property public GLFWwindow* window()
-	{
-		return m_window;
-	}
-
-	static this()
-	{
-		DerelictGL3.load();
-		DerelictGLFW3.load();
-	}
-
-	~this()
-	{
-		glfwTerminate();
-	}
-
-	bool createWindow()
-	{
-		// Setup window
-		glfwSetErrorCallback(&error_callback);
-		if (!glfwInit()){
-			return false;
-		}
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
-
-		m_window = glfwCreateWindow(1280, 720, "ImGui OpenGL3 example", null, null);
-		if(!m_window){
-			return false;
-		}
-		glfwMakeContextCurrent(m_window);
-		glfwInit();
-		DerelictGL3.reload();
-		return true;
-	}
-
-	void getWindowSize(ref int w, ref int h)
-	{
-		glfwGetWindowSize(m_window, &w, &h);
-	}
-
-	bool loop()
-	{
-		if(glfwWindowShouldClose(m_window)){
-			return false;
-		}
-		glfwPollEvents();
-		return true;
-	}
-
-	void flush()
-	{
-		glfwSwapBuffers(m_window);
-	}
-}
 
 
 void gui(
@@ -130,29 +57,27 @@ void gui(
 
 void main()
 {
+	// window & opengl
 	auto scope glfw=new GLFW();
 	if(!glfw.createWindow()){
 		return;
 	}
 
+	// gui
 	auto scope imgui=new igGlfwGL3();
 	if(!imgui.initialize(glfw.window)){
 		return;
 	}
-
 	bool show_test_window = true;
 	bool show_another_window = false;
     float[3] clear_color = [0.3f, 0.4f, 0.8f];
 
-	// Main loop
+
+	// main loop
 	while (glfw.loop())
 	{
 		// Rendering
-		int w, h;
-		glfw.getWindowSize(w, h);
-		glViewport(0, 0, w, h);
-        glClearColor(clear_color[0], clear_color[1], clear_color[2], 0);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glfw.clearRenderTarget(clear_color);
 
 		// gui
 		imgui.newFrame();
---
+++ source/glfw.d
@@ -0,0 +1,87 @@
+import derelict.opengl3.gl3;
+import derelict.glfw3.glfw3;
+
+
+extern(C) nothrow void error_callback(int error, const(char)* description)
+{
+	import std.stdio;
+    import std.conv;
+	try writefln("glfw err: %s ('%s')",error, to!string(description));
+	catch{}
+}
+
+
+class GLFW
+{
+	GLFWwindow *m_window;
+	@property public GLFWwindow* window()
+	{
+		return m_window;
+	}
+
+	static this()
+	{
+		DerelictGL3.load();
+		DerelictGLFW3.load();
+	}
+
+	~this()
+	{
+		glfwTerminate();
+	}
+
+	bool createWindow()
+	{
+		// Setup window
+		glfwSetErrorCallback(&error_callback);
+		if (!glfwInit()){
+			return false;
+		}
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
+
+		m_window = glfwCreateWindow(1280, 720, "ImGui OpenGL3 example", null, null);
+		if(!m_window){
+			return false;
+		}
+		glfwMakeContextCurrent(m_window);
+		glfwInit();
+		DerelictGL3.reload();
+		return true;
+	}
+
+	void getWindowSize(ref int w, ref int h)
+	{
+		glfwGetWindowSize(m_window, &w, &h);
+	}
+
+	bool loop()
+	{
+		if(glfwWindowShouldClose(m_window)){
+			return false;
+		}
+		glfwPollEvents();
+		return true;
+	}
+
+	void clearRenderTarget(float[] clear_color)
+	{
+		int w, h;
+		glfwGetWindowSize(m_window, &w, &h);
+		glViewport(0, 0, w, h);
+		if(clear_color.length>=3){
+			glClearColor(clear_color[0], clear_color[1], clear_color[2], 0);
+		}
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+
+	void flush()
+	{
+		glfwSwapBuffers(m_window);
+	}
+}
+
+
+
